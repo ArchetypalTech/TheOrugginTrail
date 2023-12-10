e10e8c241e2e1fad29b9deb81117217f9d91375c
timS <lbdl_kb@protonmail.com>
2023-12-10 16:59:00 +0000

    Move dynamic field to last field for Room table

--- packages/contracts/mud.config.ts
+++ packages/contracts/mud.config.ts
@@ -1,49 +1,49 @@
 import { mudConfig } from "@latticexyz/world/register";
 
 export default mudConfig({
-  enums: {
-      RoomType: ["transport", "actionable"],
-      ActionType: ["move", "mutate", "describe"]
-  },
-  tables: {
-     Room: {
-         keySchema: {
-             roomId: "uint32",
-         },
-         valueSchema: {
-             textDefId: "uint32",
-             actions: "uint32",
-             roomType: "Roomtype"
-         },
-      },
-    Action: {
-        keySchema: {
-            actionId: "uint32",
+    enums: {
+        RoomType: ["Transport", "Actionable"],
+        ActionType: ["Move", "Mutate", "Describe"],
+    },
+    tables: {
+        Room: {
+            keySchema: {
+                roomId: "uint32",
+            },
+            valueSchema: {
+                textDefId: "uint32",
+                roomType: "RoomType",
+                actions: "bytes",
+            },
         },
-        valueSchema: {
-            responseType: "ResponseType",
+        Action: {
+            keySchema: {
+                actionId: "uint32",
+            },
+            valueSchema: {
+                actionType: "ActionType",
+            },
+        },
+        TextDef: {
+            keyScheme: {
+                textId: "uint32",
+            },
+            valueSchema: {
+                description: "string",
+            },
+        },
+        Counter: {
+            keySchema: {},
+            valueSchema: "uint32",
+        },
+        History: {
+            keySchema: {
+                counterValue: "uint32",
+            },
+            valueSchema: {
+                blockNumber: "uint256",
+                time: "uint256",
+            },
         },
     },
-    TextDef: {
-        keyScheme: {
-            textId: "uint32",
-        },
-        valueSchema: {
-            description: "string",
-        },
-    },
-    Counter: {
-      keySchema: {},
-      valueSchema: "uint32",
-    },
-    History: {
-        keySchema: {
-            counterValue: "uint32",
-        },
-        valueSchema: {
-            blockNumber: "uint256",
-            time: "uint256",
-        },
-    },
-  },
 });
