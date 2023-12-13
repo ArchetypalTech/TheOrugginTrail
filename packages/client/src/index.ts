f289eb80b09cd77e0b50f585c3153814189c5532
mcbontempi@gmail.com <mcbontempi@gmail.com>
2023-12-13 14:05:42 +0000

    typing increment in the terminal calls the increment function as a test

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -5,42 +5,35 @@ const {
   components,
   systemCalls: { increment },
   network,
-} = await setup().then(async (result) => {
+} = await setup();
 
-$('body').terminal({
-    title: function(...args) {
-        const options = $.terminal.parse_options(args);
-        return fetch(options.url || 'https://terminal.jcubic.pl')
-            .then(r => r.text())
-            .then(html => html.match(/<title>([^>]+)<\/title>/)[1]);
+    $('body').terminal(async function(command)  {
+    // sanity test for calling the contract
+    if(command == 'increment') {
+        increment(command);
+    } else {
+
+
+
     }
-}, {
-    checkArity: false,
-    greetings: '# TheOrugginTrail\nAn experiment in fully onchain text adventures\n'
-});
 
-github('jcubic/jquery.terminal');
 
-});
+
+    }, { prompt: '>', name: 'TheOrugginTrail',  greetings: '# TheOrugginTrail\nAn experiment in fully onchain text adventures\n' }
+);
 
 // Components expose a stream that triggers when the component is updated.
 components.Counter.update$.subscribe((update) => {
   const [nextValue, prevValue] = update.value;
   console.log("Counter updated", update, { nextValue, prevValue });
-  document.getElementById("counter")!.innerHTML = String(nextValue?.value ?? "unset");
+//  document.getElementById("counter")!.innerHTML = String(nextValue?.value ?? "unset");
 });
 
 components.Room.update$.subscribe((update) => {
   const Room = components.Room
   console.log("Room updated");
-  document.getElementById("room_description")!.innerHTML = "We need to get the update text here";
+//  document.getElementById("room_description")!.innerHTML = "We need to get the update text here";
 });
-
-// Just for demonstration purposes: we create a global function that can be
-// called to invoke the Increment system contract via the world. (See IncrementSystem.sol.)
-(window as any).increment = async () => {
-  console.log("new counter value:", await increment());
-};
 
 (window as any).showDescription = async () => {
   console.log("looking for description:", await describe())
--- packages/contracts/src/systems/IncrementSystem.sol
+++ packages/contracts/src/systems/IncrementSystem.sol
@@ -6,14 +6,16 @@ import { Counter } from "../codegen/index.sol";
 import { Room, RoomData, Action, TextDef, History, HistoryData } from "../codegen/index.sol";
 
 contract IncrementSystem is System {
-  function increment() public returns (uint32) {
+  function increment( ) public returns (uint32) {
       uint32 counter = Counter.get();
       uint32 newValue = counter + 1;
       Counter.set(newValue);
       return newValue;
   }
+
+
 }
- 
+
 contract DescribeRoom is System {
   function desribe() public returns (string memory) {
     return "FOO-RETURN";
