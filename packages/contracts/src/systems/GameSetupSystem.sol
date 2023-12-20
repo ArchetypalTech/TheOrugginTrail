406d8ad086c704eb259814d347a4ac1e1dd73034
mcbontempi@gmail.com <mcbontempi@gmail.com>
2023-12-20 14:26:48 +0000

    make dirid automatic

--- packages/contracts/src/systems/GameSetupSystem.sol
+++ packages/contracts/src/systems/GameSetupSystem.sol
@@ -14,6 +14,8 @@ import {ActionType, RoomType, ObjectType, CommandError, DirectionType} from "../
 // dev tooling
 contract GameSetupSystem is System {
 
+    uint32 dirId = 0;
+
     function init() public returns (uint32) {
         setupWorld();
         // we are right now initing the data in the
@@ -26,35 +28,27 @@ contract GameSetupSystem is System {
         uint32 KBarn = 1;
         uint32 KMountainPath = 2;
 
-
         // plain has two exits, the mountain path and the barn
-        uint32 KPlainToBarnDirId = 0;
-        uint32 KPlainToMountainPathDirId = 1;
-        createDir(KPlainToBarnDirId, DirectionType.North, KBarn);
-        createDir(KPlainToMountainPathDirId, DirectionType.East, KMountainPath);
         uint32[] memory plainDirs = new uint32[](2);
-        plainDirs[0] = KPlainToBarnDirId;
-        plainDirs[1] = KPlainToMountainPathDirId;
+        plainDirs[0] = createDir(DirectionType.North, KBarn);
+        plainDirs[1] = createDir(DirectionType.East, KMountainPath);
         createRoom(KPlain, 'You are on a plain with the wind blowing', plainDirs);
 
         // barn has one exit, back to the plain
-        uint32 KBarnToPlainDirId = 2;
         uint32[] memory barnDirs = new uint32[](1);
-        barnDirs[0] = KBarnToPlainDirId;
-        createDir(KBarnToPlainDirId, DirectionType.South, KPlain);
+        barnDirs[0] = createDir(DirectionType.South, KPlain);
         createRoom(KBarn, 'You are in the barn', barnDirs);
 
         // mountain path has only one exit now, back to the plain
-        uint32 KMountainPathToPlainDirId = 3;
-        createDir(KMountainPathToPlainDirId, DirectionType.West, KPlain);
         uint32[] memory mountainDirs = new uint32[](1);
-        mountainDirs[0] = KMountainPathToPlainDirId;
+        mountainDirs[0] = createDir(DirectionType.West, KPlain);
         createRoom(KMountainPath, 'You are on the mountain path, you cant go any further though', mountainDirs);
     }
 
-    function createDir(uint32 dirId, DirectionType  directionType, uint32 roomId) private {
+    function createDir(DirectionType directionType, uint32 roomId) private returns (uint32){
         DirObjStore.setDirType(dirId, uint8(directionType));
         DirObjStore.setRoomId(dirId, roomId);
+        return ++dirId;
     }
 
     function createRoom(uint32 roomId, string memory description, uint32[] memory dirs) private {
