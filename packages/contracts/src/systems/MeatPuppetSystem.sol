753a5b5f09b966894610851676ca4c3b603d1b3b
timS <lbdl_kb@protonmail.com>
2023-12-21 02:00:47 +0000

    Added some more description stuff but it looks like there are no dirtypes on dirobjs

--- packages/contracts/src/systems/MeatPuppetSystem.sol
+++ packages/contracts/src/systems/MeatPuppetSystem.sol
@@ -5,7 +5,7 @@ pragma solidity >=0.8.21;
 import { console } from "forge-std/console.sol";
 
 import {System} from "@latticexyz/world/src/System.sol";
-import {Output, CurrentRoomId, RoomStore, RoomStoreData, ActionStore,  TextDef} from "../codegen/index.sol";
+import {Output, CurrentRoomId, RoomStore, RoomStoreData, ActionStore, DirObjStore, DirObjStoreData, TextDef} from "../codegen/index.sol";
 import {ActionType, RoomType, ObjectType, CommandError, DirectionType} from "../codegen/common.sol";
 import { CommandLookups } from "./CommandLookup.sol";
 import { GameConstants } from "../constants/defines.sol";
@@ -24,13 +24,37 @@ contract MeatPuppetSystem is System, GameConstants, CommandLookups  {
        _enterRoom(0); 
     }
 
+    // we are building these up but they should be attached to the DirObj ?? 
+    // but either this is wrong or we dont have any direction types set 
+    function _describeActions(uint32 rId) private returns (string memory) {
+        RoomStoreData memory currRm = RoomStore.get(rId);
+        string[8] memory dirStrings;
+        string memory msgStr;
+        for(uint8 i = 0; i < currRm.dirObjIds.length; i++) {
+            DirObjStoreData memory dir = DirObjStore.get(currRm.dirObjIds[1]);
+            if (uint8(dir.dirType) == uint8(DirectionType.North)) {
+                dirStrings[i] = "North";
+            }else if (uint8(dir.dirType) == uint8(DirectionType.East)) {
+                dirStrings[i] = "East";
+            }else {
+                dirStrings[i] = "to Hell";
+            }
+        }
+        for(uint16 i = 0; i < dirStrings.length; i++) {
+            msgStr = string(abi.encodePacked(msgStr, " ", dirStrings[i]));
+        }
+        return msgStr;
+    }
+
     function _enterRoom(uint32 rId) private returns (uint32 err) {
         // TODO:  on entering describe the room
         CurrentRoomId.set(rId);
-
-        
         RoomStoreData memory currRoom = RoomStore.get(CurrentRoomId.get());
-        Output.set(currRoom.description);
+        string memory actions = _describeActions(rId);
+        string memory pack = string(abi.encodePacked(currRoom.description, "\n", 
+                                                      "You can go ", _describeActions(rId))
+                                   );
+        Output.set(pack);
 
         return 0;
     }
