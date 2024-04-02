72c5f8f2c1f3d1be29a4b69f0543d43f73293c7a
timS <lbdl_kb@protonmail.com>
2024-04-02 18:36:37 +0100

    patch in lit-term

--- packages/client/src/lit-term.ts
+++ packages/client/src/lit-term.ts
@@ -1,53 +1,62 @@
 import { LitElement, html, css } from 'lit';
 
 class LitTerminal extends LitElement {
-
+  declare inputValue: string;
+  declare history: Array<string>;
+  static get properties() {
+    return {
+      inputValue: { type: String },
+      history: {type: Array}
+    };
+  }
   // static properties = (inputValue: {type: String}, history: {type: Array});
   constructor() {
     super();
-    this.inputValue = ''; // Initialize your property
-    this.history = ["Welcome to DEATH, well possibly...", "The O'Ruggin Trail, no:23"];
+    this.inputValue = '> '; // Initialize your property
+    this.history = ["Welcome to DEATH, well possibly...", "The O'Ruggin Trail, no:23", "An experiment in on-chain text adventures"];
   }
 
   static styles = css`
-      :host {
-          display: block;
-          min-height: 50px;
-      }
-      input {
-          padding: 8px;
-          background: black;
-          border: 1px solid chartreuse;
-          border-radius: 4px;
-          font-family: 'Arial', sans-serif;
-          font-size: 16px;
-      }
+    :host {
+      display: block;
+      min-height: 50px;
+    }
+
+    input {
+      color: forestgreen;
+      background: black;
+      outline: none;
+      border: black;
+      font-family: 'Courier', sans-serif;
+      font-size: 16px;
+      width: 100%;
+      box-sizing: border-box;
+    }
+    .terminal {
+      color: forestgreen;
+      background: black;
+      border: 1px solid chartreuse;
+      border-radius: 4px;
+      font-family: 'Courier', sans-serif;
+      font-size: 16px;
+    }
   `;
-
-  static get properties() {
-    return {
-      inputValue: { type: String }
-    };
-  }
-
-  // @state() private command = '';
-  // @state() private history: string[] = [];
-  // @state private placeHolder: string = 'Type Here:>';
 
   render() {
     return html`
       <div class="terminal">
         ${this.history.map(line => html`<div>${line}</div>`)}
-        <input type="text" placeholder=">" .value="${this.inputValue}" @keydown="${this.handleEnter}" @input=${this.handleInput}>
+        <input type="text" .value="${this.inputValue}" @keydown="${this.handleEnter}" @input=${this.handleInput}>
       </div>
     `;
   }
   private handleInput(e: Event) {
     const input = e.target as HTMLInputElement;
-    const elem : EventTarget = e.target;
+    const elem = e.target as EventTarget;
     let newVal = input.value;
-    if (this.inputValue === newVal && this.inputValue !== elem.value) {
-      elem.value = this.inputValue;
+    // @ts-ignore
+    if (this.inputValue === newVal && this.inputValue !== (elem as HTMLInputElement ).value) {
+      (elem as HTMLInputElement).value = this.inputValue;
     } else {
       this.inputValue = newVal;
     }
@@ -57,12 +66,9 @@ class LitTerminal extends LitElement {
   private handleEnter(e: KeyboardEvent) {
     if (e.key === 'Enter') {
       this.history = [...this.history, this.inputValue];
-      this.inputValue = '';
+      this.inputValue = '> ';
     }
   }
-  //
-  // private updateCommand(e: Event) {
-  //   this.command = (e.target as HTMLInputElement).value;
-  // }
+
 }
 customElements.define('l-terminal', LitTerminal);
